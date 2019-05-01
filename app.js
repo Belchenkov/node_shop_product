ec3120dfb250ddccac64062189de3d42f46cdbd8
Aleksey Belchenkov <u608110@gmail.com>
2019-05-01 19:08:22 +0300

    Helper Root Path

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -13,17 +14,11 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Server Works'
-    });
-});
-
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);
 
 app.use((req, res) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 // Run Server
--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    console.log(rootDir);
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 
