8e485a950ac62267bfbc39c4219f534f307ec837
Aleksey Belchenkov <u608110@gmail.com>
2019-05-01 14:49:58 +0300

    Add Router & Body Parser

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Routes
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT || 5000;
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.json({
         message: 'Server Works'
     });
 });
+
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on 127.0.0.1:${PORT}`);
